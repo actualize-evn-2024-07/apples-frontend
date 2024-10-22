e06f241775928bc508791a36e54cb0c1c007d802
Brian Rice <riceb53@gmail.com>
2024-10-22 09:26:00 -0700

    add router and new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import { Header } from "./Header";
 import { ApplesPage } from "./ApplesPage";
+import { ApplesNew } from "./ApplesNew";
 import { Footer } from "./Footer";
 
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <ApplesPage />,
+      },
+      {
+        path: "/apples/new",
+        element: <ApplesNew />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <ApplesPage />
-      <Footer />
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/ApplesNew.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import {useNavigate} from "react-router-dom"
+
+export function ApplesNew() {
+  const navigate = useNavigate();
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('something something')
+    const params = new FormData(event.target)
+    axios.post("http://localhost:3000/apples.json", params).then(response => {
+      navigate('/')
+    })
+  }
+  
+  return (
+    <div>
+      <p>Make a new apple</p>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <p>variety:<input type="text" name="variety" /></p>
+          <p>color:<input type="text" name="color" /></p>
+          <p>mass:<input type="text" name="mass" /></p>
+          <button>Create Apple</button>
+        </form>
+      </div>
+    </div>
+  )
+}
