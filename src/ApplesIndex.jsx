3aa14d92778c58553e120826b31d51a42f102641
Brian Rice <riceb53@gmail.com>
2024-11-05 17:54:58 -0800

    style apples index with columns

--- src/ApplesIndex.jsx
+++ src/ApplesIndex.jsx
@@ -15,17 +15,16 @@ export function ApplesIndex() {
   return (
     <div>
       <h1>Apples index</h1>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      {apples.map(apple => (
-        <div key={apple.id}>
-          <p><b>variety:</b>{apple.variety}</p>
-          <p><b>color:</b>{apple.color}</p>
-          <p><b>mass:</b>{apple.mass}</p>
-          <hr />
-        </div>
-      ))}
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-3'>
+        {apples.map(apple => (
+          <div key={apple.id} className='shadow-md rounded-md mb-4'>
+            <p><b>variety:</b>{apple.variety}</p>
+            <p><b>color:</b>{apple.color}</p>
+            <p><b>mass:</b>{apple.mass}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
